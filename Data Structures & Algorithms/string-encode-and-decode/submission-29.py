e65874e68bc01a1720886d24e430fdaf17b934f5
mud-3 <165483973+mud-3@users.noreply.github.com>
2026-06-13 19:48:06 +0800

    Add: string-encode-and-decode - submission-29

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-29.py
@@ -0,0 +1,30 @@
+class Solution:
+#solution using ascii value
+    def encode(self, strs: List[str]) -> str:
+        if strs == []:
+            return " "
+        encoded = []
+        for string in strs:
+            ascii_values = [str(ord(char)) for char in string]
+            encoded.append(", ".join(ascii_values))
+        return "; ".join(encoded)
+
+    def decode(self, s: str) -> List[str]:
+        if s == " ":
+            return []
+        ss = s.split('; ')
+        decoded = []
+        for values in ss:
+            values = values.split(', ')
+            expanded = []
+            for char in values:
+                if char == "":
+                    expanded.append("")
+                else:
+                    expanded.append(chr(int(char)))
+
+            word = ""
+            for char in expanded:
+                word += char
+            decoded.append(word)
+        return decoded
