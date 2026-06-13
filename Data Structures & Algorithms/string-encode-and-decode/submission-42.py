320030194250ab54e3ded35e6a3878b14da48822
mud-3 <165483973+mud-3@users.noreply.github.com>
2026-06-13 19:55:06 +0800

    Add: string-encode-and-decode - submission-42

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-42.py
@@ -0,0 +1,23 @@
+class Solution:
+#solution using ascii value
+    def encode(self, strs: List[str]) -> str:
+        if not strs:
+            return "[]"
+            
+        return ";".join(
+            ",".join(str(ord(char)) for char in string) for string in strs
+        )
+
+    def decode(self, s: str) -> List[str]:
+        if s == "[]":
+            return []
+
+        decoded = []
+        for asciis in s.split(';'):
+            if asciis == "":
+                decoded.append("")
+            else:
+                decoded.append(
+                    "".join(chr(int(char)) for char in asciis.split(","))
+                )
+        return decoded
