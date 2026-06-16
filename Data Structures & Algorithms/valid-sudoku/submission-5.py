283a3f596609f05c76151b694d9b2ac8755e509c
mud-3 <165483973+mud-3@users.noreply.github.com>
2026-06-16 22:34:15 +0800

    Add: valid-sudoku - submission-5

---
+++ Data Structures & Algorithms/valid-sudoku/submission-5.py
@@ -0,0 +1,30 @@
+class Solution:
+    #Bitmasking method
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        #create initial list to record upcoming condition, note that we are using binary
+        rows = [0] * 9
+        columns = [0] * 9
+        squares = [0] * 9
+
+        for i in range(9):
+            for j in range(9):
+                cell = board[i][j]
+                if cell == ".":
+                    continue
+
+                #create a binary mask by shifting 1 by (cell value - 1) places to fit 0-indexed bits.
+                #e.g. if cell = "5", shift by 4 places, mask = 00010000 (representing the 5th digit)
+                mask = 1 << int(cell) - 1
+                square = i // 3 * 3 + j // 3
+
+                #use and operation to check is that digit already been recorded (duplicate)
+                if (rows[i] & mask) or (columns[j] & mask) or (squares[square] & mask):
+                    return False
+
+                #use or operation to record that bit as 1 (visited)
+                rows[i] |= mask
+                columns[j] |= mask
+                squares[square]|= mask
+        
+        return True
