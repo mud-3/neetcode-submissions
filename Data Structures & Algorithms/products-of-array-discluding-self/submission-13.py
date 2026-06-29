d2dc06549b710d6689821f000f577d82d21811f9
mud-3 <165483973+mud-3@users.noreply.github.com>
2026-06-29 23:53:12 +0800

    Add: products-of-array-discluding-self - submission-13

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-13.py
@@ -0,0 +1,15 @@
+class Solution:
+    #optimal solution
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answer = [0] * len(nums)
+        preproducts = postproducts = 1
+
+        for i in range(len(nums)):
+            answer[i] = preproducts
+            preproducts *= nums[i]
+        
+        for i in range(len(nums) - 1, -1, -1):
+            answer[i] *= postproducts
+            postproducts *= nums[i]
+
+        return answer
