244753cd271e420dfdd5845c965d966e87d302f0
mud-3 <165483973+mud-3@users.noreply.github.com>
2026-08-13 22:39:17 +0800

    Add: daily-temperatures - submission-26

---
+++ Data Structures & Algorithms/daily-temperatures/submission-26.py
@@ -0,0 +1,14 @@
+class Solution:
+    #optimised O(n) solution with stack
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        output = [0] * len(temperatures)
+
+        for i, temperature in enumerate(temperatures):
+            while stack and stack[-1][1] < temperature:
+                index = stack.pop()[0]
+                output[index] = (i - index)
+
+            stack.append((i, temperature))
+
+        return output
